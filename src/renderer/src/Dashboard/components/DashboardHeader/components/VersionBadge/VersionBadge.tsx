a52d3514a95a20c1036f0e146f297da94b5f4ab7
MapleYeti <mapleyeti@proton.me>
2025-08-19 23:31:34 -0400

    update versioning to 0.2.0

--- src/renderer/src/Dashboard/components/DashboardHeader/components/VersionBadge/VersionBadge.tsx
+++ src/renderer/src/Dashboard/components/DashboardHeader/components/VersionBadge/VersionBadge.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './VersionBadge.module.css'
 
 const VersionBadge: React.FC = () => {
-  return <div className={styles.versionBadge}>v0.1.2</div>
+  return <div className={styles.versionBadge}>v0.2.0</div>
 }
 
 export default VersionBadge
